521244f1b6f866a143bfdbf6554931e20ae7b55a
ParkJongHoon <hom8181@naver.com>
2024-02-13 16:51:20 +0900

    Title: 장학금, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240213/장학금/scholarship.java
+++ 240213/장학금/scholarship.java
@@ -9,9 +9,9 @@ public class Main {
 
         if (a >= 90) {
             if (b >= 95) {
-                System.out.print(10);
+                System.out.print(100000);
             } else if (b >= 90) {
-                System.out.print(5);
+                System.out.print(50000);
             } else {
                 System.out.print(0);
             }
