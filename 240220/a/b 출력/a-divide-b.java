526a742065028c98cb28456c238daf4dac03cfb8
ParkJongHoon <hom8181@naver.com>
2024-02-20 18:33:08 +0900

    Title: a/b 출력, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240220/a/b 출력/a-divide-b.java
+++ 240220/a/b 출력/a-divide-b.java
@@ -6,8 +6,9 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        a %= b;
         System.out.print(a / b + ".");
+        a %= b;
+
         for (int i = 0; i < 20; i++) {
             a *= 10;
             System.out.print(a / b);
